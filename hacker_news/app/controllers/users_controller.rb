790a1f6a520825b854c11caa8bfee386d00250a6
Jonathan Chung <hukashi@gmail.com>
2015-07-16 12:20:20 -0400

    add get and create routes for new user

--- hacker_news/app/controllers/users_controller.rb
+++ hacker_news/app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = @User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path
+    else
+      flash[:errors] = user.errors.full_messages
+      redirect_to new_user_post_path
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(username:, :first_name, :last_name, :password)
+  end
+
 end
